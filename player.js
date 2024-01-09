ab62be22b14bf4c2d9de015dee3b7c522ab3c901
Hygor Marques <68515062+Hygor0206@users.noreply.github.com>
2024-01-09 17:10:19 -0300

    Player almost done

--- player.js
+++ player.js
@@ -1,6 +1,7 @@
 let albumCoverArt = null;
 let trackTitle = null;
 let artist = null;
+
 function wallpaperMediaPropertiesListener(event) {
 	// Update title and artist labels
 	trackTitle.textContent = event.title;
@@ -8,20 +9,21 @@ function wallpaperMediaPropertiesListener(event) {
 }
 function wallpaperMediaThumbnailListener(event) {
 	// Update album cover art
-	if(event){
-        albumCoverArt.src = event.thumbnail;
-    }else{
+	if (event.thumbnail === 'data:image/png;base64,') {
         albumCoverArt.src = "img/blank_song.jpg";
+    } else {
+        albumCoverArt.src = event.thumbnail;
     }
 	document.body.style['background-color'] = event.primaryColor;
 	trackTitle.style.color = event.textColor;
 	artist.style.color = event.textColor;
 }
+
 // Find all required elements
 albumCoverArt = document.getElementById('albumCoverArt');
 trackTitle = document.getElementById('trackTitle');
 artist = document.getElementById('artist');
-// Register the media property listener provided by Wallpaper Engine.
+
 window.wallpaperRegisterMediaPropertiesListener(wallpaperMediaPropertiesListener);
 // Register the media thumbnail listener provided by Wallpaper Engine.
 +window.wallpaperRegisterMediaThumbnailListener(wallpaperMediaThumbnailListener);
